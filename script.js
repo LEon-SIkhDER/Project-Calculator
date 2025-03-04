162f922beba074ba47a939a2867b727326ae8f4b
LEon-SIkhDER <leondesktop7580@gmail.com>
2025-03-04 13:10:31 +0600

    main js will be start

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.getElementById("calculate").addEventListener("click", function(){
-    const input = document.getElementById("input").value
-    const result = eval(input)
-    document.getElementById("input").value = result
-    console.log(result)
+// document.getElementById("calculate").addEventListener("click", function(){
+//     const input = document.getElementById("input").value
+//     const result = eval(input)
+//     document.getElementById("input").value = result
+
+
+// })
+
+document.getElementById("container").addEventListener("click", function(e){
+    console.log(e.target)
 })
