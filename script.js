6b19e896de6c236b8f99c2ae5faadfedbb426827
LEon-SIkhDER <leondesktop7580@gmail.com>
2025-03-04 15:10:50 +0600

    done without del button

--- script.js
+++ script.js
@@ -6,6 +6,30 @@
 
 // })
 
-document.getElementById("container").addEventListener("click", function(e){
-    console.log(e.target)
+document.getElementById("container").addEventListener("click", function (e) {
+
+    const value = e.target.innerText
+    const input = document.getElementById("input").value
+    console.log(value)
+    console.log(typeof value)
+    if (value === "AC") {
+        document.getElementById("input").value = ""
+        return
+    }
+    if (value === "DEL") {
+        return
+    }
+    // return 
+    if (value === "=") {
+        const result = eval(input)
+        console.log(result)
+        document.getElementById("input").value = result
+
+
+    }
+    else {
+        document.getElementById("input").value += value
+        
+    }
+
 })
