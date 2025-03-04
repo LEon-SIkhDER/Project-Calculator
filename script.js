44b27a0f58f4c097dd36ed66d5bd4e6692a39bc4
LEon-SIkhDER <leondesktop7580@gmail.com>
2025-03-04 18:07:41 +0600

    functional del button

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-// document.getElementById("calculate").addEventListener("click", function(){
-//     const input = document.getElementById("input").value
-//     const result = eval(input)
-//     document.getElementById("input").value = result
-
-
-// })
-
 document.getElementById("container").addEventListener("click", function (e) {
-
     const value = e.target.innerText
     const input = document.getElementById("input").value
-    console.log(value)
-    console.log(typeof value)
     if (value === "AC") {
         document.getElementById("input").value = ""
         return
     }
     if (value === "DEL") {
+        document.getElementById("input").value = input.slice(0, -1)
+        console.log(input)
         return
     }
     // return 
@@ -31,5 +22,4 @@ document.getElementById("container").addEventListener("click", function (e) {
         document.getElementById("input").value += value
         
     }
-
 })
